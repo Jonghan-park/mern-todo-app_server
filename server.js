ed016920aed8e77ca3c03130cf00bbc4e17d7a9b
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-30 12:47:20 -0600

    Update server

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.post('/todo/new', (req, res) => {
   res.json(todo);
 })
 
-app.delete('/todo/delete/:unique_identifier', async (req, res) => {
-  const result = await Todo.findByIdAndDelete(req.params.unique_identifier)
+app.delete('/todo/delete/:id', async (req, res) => {
+  const result = await Todo.findByIdAndDelete(req.params.id)
 
   res.json(result);
 })
