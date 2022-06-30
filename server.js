097cae58e49dd060b2d657f9d86e4ab2915edada
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-30 13:26:51 -0600

    Update server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 require('dotenv').config();
-const cors = require('cors')
+const cors = require('cors');
 const port = process.env.PORT || 5000;
-const Todo = require('./medels/Todo')
+const Todo = require('./medels/Todo');
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connected to DB'))
@@ -17,32 +17,41 @@ mongoose.connect(process.env.MONGO_URI)
 app.get('/todos', async (req, res) => {
   const todos = await Todo.find();
 
-  res.json(todos)
-})
+  res.json(todos);
+});
 
 app.post('/todo/new', (req, res) => {
   const todo = new Todo({
     text:req.body.text
-  })
+  });
 
   todo.save();
 
   res.json(todo);
-})
+});
 
 app.delete('/todo/delete/:id', async (req, res) => {
-  const result = await Todo.findByIdAndDelete(req.params.id)
+  const result = await Todo.findByIdAndDelete(req.params.id);
 
-  res.json(result);
-})
+  res.json({result});
+});
 
 app.get('/todo/complete/:id', async (req, res) => {
-  const todo = await Todo.findById(req.params.id)
+  const todo = await Todo.findById(req.params.id);
 
   todo.complete = !todo.complete;
   todo.save();
   
   res.json(todo);
-})
+});
+app.put('/todo/update/:id', async (req, res) => {
+	const todo = await Todo.findById(req.params.id);
 
-app.listen(port, () => console.log("Server started on port 5000"))
+	todo.text = req.body.text;
+
+	todo.save();
+
+	res.json(todo);
+});
+
+app.listen(port, () => console.log("Server started on port 5000"));
