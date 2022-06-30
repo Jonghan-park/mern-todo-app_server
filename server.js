d2fb215cc28a8158cd68e9d5350b4dcb62077ac7
Jonghan Park <jonghan.park@edu.sait.ca>
2022-06-30 11:12:21 -0600

    Update todo

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
+require('dotenv').config();
 const cors = require('cors')
+const port = process.env.PORT || 5000;
+const Todo = require('./medels/Todo')
 
 const app = express()
 
@@ -10,3 +13,11 @@ app.use(cors())
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connected to DB'))
 .catch(console.error);
+
+app.get('./todos', async (req, res) => {
+  const todos = await Todo.find();
+
+  res.json(todos)
+})
+
+app.listen(port, () => console.log("Server started on port 5000"))
